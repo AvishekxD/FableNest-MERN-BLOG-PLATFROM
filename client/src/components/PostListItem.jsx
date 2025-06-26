fcac73bcd634463ce9acf48421f11d223c671449
AvishekxD <abmeena00@gmail.com>
2025-06-26 08:33:02 +0530

    feat(post): fetching  single post data using React Query

--- client/src/components/PostListItem.jsx
+++ client/src/components/PostListItem.jsx
@@ -3,29 +3,29 @@ import Imag from "./Imag";
 import { format } from "timeago.js";
 
 const PostListItem = ({ post }) => {
-  return (
-    <div className="flex flex-col xl:flex-row gap-8 mb-12">
-        
-       {post.img && <div className="md:hidden xl:block xl:w-2/3">
-            <Imag src={post.img} className="rounded-2xl object-cover" w="750"/>
-        </div>}
-        
-        <div className="flex flex-col gap-4 xl:w-3/3">
-            <Link to={`/${post.slug}`} className="text-4xl font-semibold">
-                {post.title}
-            </Link>
-            <div className="flex items-center gap-2 text-zinc-700 text-sm">
-                <span>Written by</span>
-                <Link className="text-zinc-200" to={`/posts?author=${post.user.username}`}>{post.user.username}</Link>
-                <span>on</span>
-                <Link className="text-zinc-200">{post.category}</Link>
-                <span>{format(post.createdAt)}</span>
+    return (
+        <div className="flex flex-col xl:flex-row gap-8 mb-12">
+            
+        {post.img && <div className="md:hidden xl:block xl:w-2/3">
+                <Imag src={post.img} className="rounded-2xl object-cover" w="750"/>
+            </div>}
+            
+            <div className="flex flex-col gap-4 xl:w-3/3">
+                <Link to={`/${post.slug}`} className="text-4xl font-semibold">
+                    {post.title}
+                </Link>
+                <div className="flex items-center gap-2 text-zinc-700 text-sm">
+                    <span>Written by</span>
+                    <Link className="text-zinc-200" to={`/posts?author=${post.user.username}`}>{post.user.username}</Link>
+                    <span>on</span>
+                    <Link className="text-zinc-200">{post.category}</Link>
+                    <span>{format(post.createdAt)}</span>
+                </div>
+                <p>{post.desc}</p>
+                <Link to={`/${post.slug}`} className="underline text-indigo-200 text-sm">Read More</Link>
             </div>
-            <p>{post.desc}</p>
-            <Link to={`/${post.slug}`} className="underline text-indigo-200 text-sm">Read More</Link>
         </div>
-    </div>
-  )
+    )
 }
 
 export default PostListItem
--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -1,135 +1,76 @@
 import Imag from "../components/Imag"
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import PostMenuActions from "../components/PostMenuActions"
 import Search from "../components/Search"
 import Comments from "../components/Comments"
+import axios from "axios"
+import { useQuery } from "@tanstack/react-query"
+import { format } from "timeago.js"
+
+const fetchPost = async (slug) => {
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts/${slug}`);
+  return res.data;
+};
+
 
 const SinglePostPage = () => {
+
+  const { slug } = useParams();
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["post", slug],
+    queryFn: () => fetchPost(slug),
+  });
+
+  if (isPending) return "loading...";
+  if (error) return "Something went wrong!" + error.message;
+  if (!data) return "Post not found!";
+
   return (
     <div className="flex flex-col gap-8 mt-4">
       {/* detial */}
       <div className="flex gap-8">
         <div className="lg:w-3/5 flex flex-col gap-8">
         <h1 className="text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          {data.title}
         </h1>
         <div className="flex items-center gap-2 text-[var(--Accent)] text-sm">
           <span>Written by</span>
-          <Link className="text-indigo-300">John Doe</Link>
+          <Link className="text-indigo-300">{data.user.username}</Link>
           <span>on</span>
-          <Link>Photography</Link>
-          <span>2 Days ago</span>
+          <Link className="text-zinc-200">{data.category}</Link>
+          <span>{format(data.createdAt)}</span>
         </div>
         <p className="text-[var(--Accent)] font-medium">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta, accusantium 
-          quod recusandae dolorem temporibus expedita praesentium dolore repellendus labore 
-          nam voluptates ipsa molestias, velit laudantium consequuntur! Expedita facere labore 
-          iure accusantium!
+          {data.desc}
         </p>
         </div>
-        <div className="hidden lg:block w-2/5">
-          <Imag src="Sunset.jpg" w="800" className="rounded-2xl"/>
-        </div>
+        {data.img && (<div className="hidden lg:block w-2/5">
+          <Imag src={data.img} w="800" className="rounded-2xl"/>
+        </div>)}
       </div>
       {/* content */}
       <div className="flex flex-col md:flex-row gap-10">
         {/* text */}
         <div className="lg:text:lg flex flex-col gap-6 text-justify">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eaque eligendi unde. 
-            Perferendis ipsa rerum fugiat necessitatibus sunt similique nostrum libero asperiores qui dolores 
-            magnam dolore consequuntur quam odio nemo, facilis repellendus, quidem dignissimos exercitationem 
-            expedita voluptas sequi, totam corrupti! Placeat recusandae deserunt mollitia sint quasi nobis 
-            molestiae modi aliquid quod aliquam, suscipit quo vel neque adipisci excepturi delectus quidem, 
-            provident repudiandae itaque eligendi, molestias est. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Distinctio ex pariatur fuga odio officia ullam, veniam quia similique labore culpa? Lorem 
-            ipsum dolor sit amet consectetur adipisicing elit. Recusandae, corrupti?
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eaque eligendi unde. 
-            Perferendis ipsa rerum fugiat necessitatibus sunt similique nostrum libero asperiores qui dolores 
-            magnam dolore consequuntur quam odio nemo, facilis repellendus, quidem dignissimos exercitationem 
-            expedita voluptas sequi, totam corrupti! Placeat recusandae deserunt mollitia sint quasi nobis 
-            molestiae modi aliquid quod aliquam, suscipit quo vel neque adipisci excepturi delectus quidem, 
-            provident repudiandae itaque eligendi, molestias est. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Distinctio ex pariatur fuga odio officia ullam, veniam quia similique labore culpa? Lorem 
-            ipsum dolor sit amet consectetur adipisicing elit. Recusandae, corrupti?
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eaque eligendi unde. 
-            Perferendis ipsa rerum fugiat necessitatibus sunt similique nostrum libero asperiores qui dolores 
-            magnam dolore consequuntur quam odio nemo, facilis repellendus, quidem dignissimos exercitationem 
-            expedita voluptas sequi, totam corrupti! Placeat recusandae deserunt mollitia sint quasi nobis 
-            molestiae modi aliquid quod aliquam, suscipit quo vel neque adipisci excepturi delectus quidem, 
-            provident repudiandae itaque eligendi, molestias est. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Distinctio ex pariatur fuga odio officia ullam, veniam quia similique labore culpa? Lorem 
-            ipsum dolor sit amet consectetur adipisicing elit. Recusandae, corrupti?
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eaque eligendi unde. 
-            Perferendis ipsa rerum fugiat necessitatibus sunt similique nostrum libero asperiores qui dolores 
-            magnam dolore consequuntur quam odio nemo, facilis repellendus, quidem dignissimos exercitationem 
-            expedita voluptas sequi, totam corrupti! Placeat recusandae deserunt mollitia sint quasi nobis 
-            molestiae modi aliquid quod aliquam, suscipit quo vel neque adipisci excepturi delectus quidem, 
-            provident repudiandae itaque eligendi, molestias est. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Distinctio ex pariatur fuga odio officia ullam, veniam quia similique labore culpa? Lorem 
-            ipsum dolor sit amet consectetur adipisicing elit. Recusandae, corrupti?
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eaque eligendi unde. 
-            Perferendis ipsa rerum fugiat necessitatibus sunt similique nostrum libero asperiores qui dolores 
-            magnam dolore consequuntur quam odio nemo, facilis repellendus, quidem dignissimos exercitationem 
-            expedita voluptas sequi, totam corrupti! Placeat recusandae deserunt mollitia sint quasi nobis 
-            molestiae modi aliquid quod aliquam, suscipit quo vel neque adipisci excepturi delectus quidem, 
-            provident repudiandae itaque eligendi, molestias est. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Distinctio ex pariatur fuga odio officia ullam, veniam quia similique labore culpa? Lorem 
-            ipsum dolor sit amet consectetur adipisicing elit. Recusandae, corrupti?
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eaque eligendi unde. 
-            Perferendis ipsa rerum fugiat necessitatibus sunt similique nostrum libero asperiores qui dolores 
-            magnam dolore consequuntur quam odio nemo, facilis repellendus, quidem dignissimos exercitationem 
-            expedita voluptas sequi, totam corrupti! Placeat recusandae deserunt mollitia sint quasi nobis 
-            molestiae modi aliquid quod aliquam, suscipit quo vel neque adipisci excepturi delectus quidem, 
-            provident repudiandae itaque eligendi, molestias est. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Distinctio ex pariatur fuga odio officia ullam, veniam quia similique labore culpa? Lorem 
-            ipsum dolor sit amet consectetur adipisicing elit. Recusandae, corrupti?
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eaque eligendi unde. 
-            Perferendis ipsa rerum fugiat necessitatibus sunt similique nostrum libero asperiores qui dolores 
-            magnam dolore consequuntur quam odio nemo, facilis repellendus, quidem dignissimos exercitationem 
-            expedita voluptas sequi, totam corrupti! Placeat recusandae deserunt mollitia sint quasi nobis 
-            molestiae modi aliquid quod aliquam, suscipit quo vel neque adipisci excepturi delectus quidem, 
-            provident repudiandae itaque eligendi, molestias est. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Distinctio ex pariatur fuga odio officia ullam, veniam quia similique labore culpa? Lorem 
-            ipsum dolor sit amet consectetur adipisicing elit. Recusandae, corrupti?
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eaque eligendi unde. 
-            Perferendis ipsa rerum fugiat necessitatibus sunt similique nostrum libero asperiores qui dolores 
-            magnam dolore consequuntur quam odio nemo, facilis repellendus, quidem dignissimos exercitationem 
-            expedita voluptas sequi, totam corrupti! Placeat recusandae deserunt mollitia sint quasi nobis 
-            molestiae modi aliquid quod aliquam, suscipit quo vel neque adipisci excepturi delectus quidem, 
-            provident repudiandae itaque eligendi, molestias est. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Distinctio ex pariatur fuga odio officia ullam, veniam quia similique labore culpa? Lorem 
-            ipsum dolor sit amet consectetur adipisicing elit. Recusandae, corrupti?
-          </p>
+            <div dangerouslySetInnerHTML={{ __html: data.content }} />
         </div>
         {/* menu */}
         <div className="px-4 h-max sticky top-8">
             <h1 className="mb-4 text-sm font-medium">Author</h1>
             <div className="flex flex-col gap-4 ">
               <div className="flex items-center gap-8">
-                  <Imag 
-                    src="Author.jpg" 
-                    className="w-12 h-12 rounded-full object-cover" 
-                    w="48" 
-                    h="48"
+                {data.user.img && (
+                <Imag
+                  src={data.user.img}
+                  className="w-12 h-12 rounded-full object-cover"
+                  w="48"
+                  h="48"
                   />
-                  <Link className="text-indigo-300">John Doe</Link>
+                )}
+                  <Link className="text-indigo-300">{data.user.username}</Link>
               </div>
-              <p className="text-sm text-zinc-300">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <p className="text-sm text-zinc-200">Bringing you thoughtful articles and personal experiences.</p>
               <div className="flex gap-2">
                 <Link>
                 <Imag src="facebook.svg" w="24"/>
@@ -139,7 +80,7 @@ const SinglePostPage = () => {
                 </Link>
               </div>
             </div>
-          <PostMenuActions/>
+          <PostMenuActions post={data}/>
           <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
           <div className="flex flex-col gap-2 text-sm">
             <Link className="underline">All</Link>
@@ -163,7 +104,7 @@ const SinglePostPage = () => {
           <Search/>
         </div>
         </div>
-        <Comments />
+        <Comments postId={data._id}/>
     </div>
   )
 }
